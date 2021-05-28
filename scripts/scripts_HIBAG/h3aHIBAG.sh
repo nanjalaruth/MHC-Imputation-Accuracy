e90bc722a112d45bde8bd61c179123b01e96dd07
nanjalaruth <ruthnanjala97@gmail.com>
2021-05-28 08:30:40 +0200

    modified h3aHIBAG.sh

--- scripts/scripts_HIBAG/h3aHIBAG.sh
+++ scripts/scripts_HIBAG/h3aHIBAG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name='HIBAG model for h3a population'
-#SBATCH --mem=64GB
+#SBATCH --mem=90GB
 #SBATCH --cpus-per-task=32
 #SBATCH --output=h3a-%j-stdout.log
 #SBATCH --error=h3a-%j-stderr.log
