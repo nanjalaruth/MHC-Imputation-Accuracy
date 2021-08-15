7863cc5979eaf1332beadb07cfab0be33198eca4
nanjalaruth <ruthnanjala97@gmail.com>
2021-08-15 02:08:17 +0200

    modified submission script

--- hlaimputation.sh
+++ hlaimputation.sh
@@ -5,10 +5,9 @@
 #SBATCH --mem=5GB
 #SBATCH --output=hlaimputation-%j-stdout.log
 #SBATCH --error=hlaimputation-%j-stderr.log
-#SBATCH --time=10-00:00:00
+#SBATCH --time=14-00:00:00
 
 cd /scratch3/users/nanje/hlatyping/results
 
 echo "Submitting SLURM job"
-# nextflow -c /scratch3/users/nanje/MHC-Imputation-Accuracy/nextflow.config run /scratch3/users/nanje/MHC-Imputation-Accuracy/hlaped.nf -profile singularity -resume
-nextflow -c /scratch3/users/nanje/MHC-Imputation-Accuracy/nextflow.config run /scratch3/users/nanje/MHC-Imputation-Accuracy/hlaped.nf -profile singularity,slurm -resume
+nextflow run /scratch3/users/nanje/MHC-Imputation-Accuracy/main.nf -profile singularity,slurm -resume
