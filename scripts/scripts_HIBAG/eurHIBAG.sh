7097ecba69751dc6f0fbf025fa41c5ee3877b1c2
nanjalaruth <ruthnanjala97@gmail.com>
2022-04-04 10:36:48 +0200

    modified script

--- scripts/scripts_HIBAG/eurHIBAG.sh
+++ scripts/scripts_HIBAG/eurHIBAG.sh
@@ -5,8 +5,8 @@
 #SBATCH --output=eur-%j-stdout.log
 #SBATCH --error=eur-%j-stderr.log
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=32
-#SBATCH --nodes=20
+#SBATCH --cpus-per-task=10
+#SBATCH --nodes=8
 
 echo "submitting slurm job"
-Rscript eurHIBAG.R 
+Rscript Eur_HIBAG.R 
