5216c67f13293a415fcfa0ef4ca2319840bfdd59
nanjalaruth <ruthnanjala97@gmail.com>
2022-06-14 14:04:39 +0200

    modified impute.sh

--- multiethnic/impute.sh
+++ multiethnic/impute.sh
@@ -1,3 +1,11 @@
+#split multiallelic sites
+bcftools norm -m- /cbio/projects/001/clients/hlaimpute/datasets/h3a_mhc_ggvp.vcf.gz -Ob -o h3a_splitted_vcf
+
+#phase with Eagle
+./eagle --numThreads=16 --vcf="./h3a_splitted_vcf" \
+--geneticMapFile="/cbio/dbs/refpanels/eagle/tables/genetic_map_hg19_withX.txt.gz" \
+--vcfOutFormat=z --outPrefix="h3a_eagle_phased"
+
 #Convert to m3vcf with minimac3
 minimac3 --refHaps Gwd_popref_hla.vcf.gz --processReference --prefix Gwd_popref_phased --rsid
 
