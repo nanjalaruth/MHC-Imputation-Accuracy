ea80a2d552996ed267cc4f0e4306efe92431bcf4
Nanjala Ruth <55382239+nanjalaruth@users.noreply.github.com>
2022-08-15 15:35:23 +0300

    Update and rename renamefastq.sh to renamefile.sh

--- scripts/renamefastq.sh
+++
@@ -1,8 +0,0 @@
-indir=/scratch3/users/nanje/hlatyping/h3a_hlatypes/results/optitype
-
-for file in $(ls ${indir})
-do 
-	new=${file%%.*}
-	#echo ${new}
-	mv ${indir}/${file}/${file}_result.tsv ${indir}/${file}/${new}_result.tsv
-done
