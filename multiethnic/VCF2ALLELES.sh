819408ec42ef4d3d3da325669e99184b65c36034
nanjalaruth <ruthnanjala97@gmail.com>
2022-09-16 11:54:38 +0200

    modified file

--- multiethnic/VCF2ALLELES.sh
+++ multiethnic/VCF2ALLELES.sh
@@ -1,3 +1,3 @@
 echo ID $(bcftools query -l chr6.dose.vcf.gz | tr '\n' ' ') > preprocessed.txt 
 bcftools query -f '%ID[ %GT]\n' chr6.dose.vcf.gz  | grep '^HLA' >> preprocessed.txt 
-python ../4d_VCF2ALLELES.py preprocessed.txt out
+python ../edit_4d_VCF2ALLELES.py preprocessed.txt out
